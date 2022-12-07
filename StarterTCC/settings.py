6c0d63973fe60de4e25b7214ddb8d001c6fdeca5
Waddingtonf <waddingtonf@outlook.com>
2022-12-06 23:20:38 -0300

    teste numero 132131

--- StarterTCC/settings.py
+++ StarterTCC/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,11 +92,11 @@ DEFAULT_FROM_EMAIL = 'Studio3DMockup <noreply@studio3dmockup.com>'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tccfinal_bd',
-        'USER': 'uodditu',
-        'PASSWORD': '132489Ab',
-        'HOST': 'db4free.net',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': '8pU4ug2tj9ZkAcyCvVlY',
+        'HOST': 'containers-us-west-134.railway.app',   # Or an IP Address that your DB is hosted on
+        'PORT': '5598',
     }
 }
 
@@ -157,10 +157,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
